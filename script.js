5e655f4d40e781bea643f36795906790c6783c09
Luca Tsun-Yien Cheung <lacuna64469@gmail.com>
2025-01-16 18:11:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let clicked = false;
 function funnyMove(){
-    alert("bruh");
     if (clicked){
         let button2 = document.getElementById("clicked");
         button2.innerHTML = "Click me";
         button2.id = "funnyButton";
+        alert("bruh");
     } else{
         let button1 = document.getElementById("funnyButton");
         button1.innerHTML = "Teehee";
         button1.id = "clicked";
+        alert("Hahaha");
     }
     clicked = !clicked;
 }
