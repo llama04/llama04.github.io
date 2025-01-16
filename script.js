75cffa65110ddbf1aedd94acb4a05fd6b066eb29
Luca Tsun-Yien Cheung <lacuna64469@gmail.com>
2025-01-16 17:29:47 +0000

    bruh

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 document.getElementById("funnyButton").addEventListener("click", funnyMove);
+document.write(1 + 5);
 
 function funnyMove(){
     alert("You've been alerted!");
-    var button1 = document.getElementById(funnyButton);
+    let button1 = document.getElementById("funnyButton");
     button1.innerHTML = "Teehee";
     button1.id = "clicked";
 }
